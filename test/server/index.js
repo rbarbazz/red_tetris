162a124637fc43bcba64f6e29412bb4827f2abfb
rbarbazz <raphael.barbazza@gmail.com>
2019-10-03 18:44:40 +0200

    test src/server added

--- test/server/index.js
+++ test/server/index.js
@@ -1,10 +1,12 @@
-import { expect } from 'chai';
-
 import * as server from '../../src/server/index';
+import params from '../../src/params';
 
 
 export default () => describe('Index', () => {
-  it('should', () => {
-    expect(1).to.equal(1);
+  it('should start and close the server', () => {
+    server.create(params.server)
+      .then(promise => promise.stop(() => {}))
+      // eslint-disable-next-line no-console
+      .catch(error => console.log(error));
   });
 });
